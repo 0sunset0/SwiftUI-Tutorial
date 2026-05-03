99150c58daa80c6afbad2e7d40dd677d0baf8713
0sunset0 <chi6465618@naver.com>
2026-05-03 14:31:46 +0900

    feat: Work with relationships

---
+++ tutorial/08. FriendsFavoriteMovies/08. FriendsFavoriteMovies/movie/FilteredMovieList.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredMovieList.swift
+//  08. FriendsFavoriteMovies
+//
+//  Created by 노을 on 5/3/26.
+//
+
+import SwiftUI
+
+struct FilteredMovieList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilteredMovieList()
+}
